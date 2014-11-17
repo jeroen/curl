545eafb1731f7b7452d641b4b66cf7f572ba5899
Jeroen Ooms <jeroenooms@gmail.com>
2014-11-17 13:09:54 -0800

    Add examples.

--- R/curl.R
+++ R/curl.R
@@ -1,10 +1,11 @@
 #' Curl connection
 #'
-#' Drop-in replacement for url() that uses libcurl to support https.
+#' Drop-in replacement for url() that uses libcurl. Supports http(s), ftp(s), file,
+#' gzip, deflate, etc.
 #'
 #' @useDynLib curl R_curl_connection
 #' @export
-#' @examples test <- curl()
+#' @examples test <- curl("http://httpbin.org/get")
 #' open(test)
 #' readLines(test)
 #' close(test)
@@ -22,17 +23,23 @@
 #' # Error after redirect
 #' curl("https://httpbin.org/redirect-to?url=http://httpbin.org/status/418", "r")
 #'
-#' # FTP support
-#' read.csv(curl("ftp://cran.r-project.org/pub/R/CRAN_mirrors.csv"))
-#'
-#'  # Stream large dataset over https with gzip
-#' library(jsonlite)
-#' con <- gzcon(curl("https://jeroenooms.github.io/data/nycflights13.json.gz"))
-#' nycflights <- stream_in(con)
-#'
 #' # Accept-Encoding: compress, gzip
 #' readLines(curl("http://httpbin.org/gzip))
 #' readLines(curl("http://httpbin.org/deflate))
+#'
+#' # File support
+#' test <- paste0("file://", system.file("CITATION"))
+#' readLines(curl(test))
+#'
+#' # Other protocols
+#' read.csv(curl("ftp://cran.r-project.org/pub/R/CRAN_mirrors.csv"))
+#' readLines(curl("ftps://test.rebex.net:990/readme.txt"))
+#' readLines(curl("gopher://quux.org/1"))
+#'
+#' # Stream large dataset over https with gzip
+#' library(jsonlite)
+#' con <- gzcon(curl("https://jeroenooms.github.io/data/nycflights13.json.gz"))
+#' nycflights <- stream_in(con)
 #'
 curl <- function(url = "http://httpbin.org/get", open = ""){
   .Call(R_curl_connection, url, open)
