5ca33a117511baa696441fb9a02349b5a899ebd4
Jeroen Ooms <jeroenooms@gmail.com>
2014-11-19 09:25:56 -0800

    Add script

---
+++ tools/winlibs.R
@@ -0,0 +1,5 @@
+# Build against openssl libraries that were compiled with the Rtools gcc toolchain.
+setInternet2()
+download.file("http://jeroenooms.github.io/openssl/windows/curl-7.39.0-devel.zip", "lib.zip", quiet = TRUE)
+dir.create("../windows", showWarnings = FALSE)
+unzip("lib.zip", exdir = "../windows")
