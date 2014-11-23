0d80fa794784075b209b5454fc3a998887193535
Jeroen <jeroenooms@gmail.com>
2014-11-22 19:56:21 -0800

    Fix download script

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,5 +1,8 @@
-# Build against openssl libraries that were compiled with the Rtools gcc toolchain.
-setInternet2()
-download.file("http://jeroenooms.github.io/curl/windows/curl-7.39.0-devel.zip", "lib.zip", quiet = TRUE)
-dir.create("../windows", showWarnings = FALSE)
-unzip("lib.zip", exdir = "../windows")
+# Build against static libraries from curl website.
+if(!file.exists("../windows/curl-7.39.0-devel/include/curl/curl.h")){
+  setInternet2()
+  download.file("http://jeroenooms.github.io/curl/windows/curl-7.39.0-devel.zip", "lib.zip", quiet = TRUE)
+  dir.create("../windows", showWarnings = FALSE)
+  unzip("lib.zip", exdir = "../windows")
+  unlink("lib.zip")
+}
