c577660eca296a66a27cb5120d4bf6fa60797cd8
Jeroen Ooms <jeroenooms@gmail.com>
2015-02-27 14:59:34 -0800

    Add manual

--- R/proxy.R
+++ R/proxy.R
@@ -1,3 +1,9 @@
+#' Internet Explorer Proxy Settings
+#'
+#' This function retrieves the system proxy settings on Windows as
+#' configured in Internet Explorer. This can be used to automatically
+#' configure the libcurl to use the same proxy server.
+#'
 #' @useDynLib curl R_proxy_info
 #' @export
 ie_proxy_info <- function(){
