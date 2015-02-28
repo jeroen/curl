f1e15d085d14a8fd21da1f3044be17a2a0e99349
Jeroen Ooms <jeroenooms@gmail.com>
2015-02-27 17:34:05 -0800

    Fix for cookies

--- R/handle.R
+++ R/handle.R
@@ -14,19 +14,26 @@ new_handle <- function(){
 #' @rdname handle
 #' @param handle a curl handle object
 #' @examples h <- new_handle()
-#' req <- curl_perform("http://httpbin.org/cookies/set?foo=123&bar=ftw", handle = h)
+#' get_handle_cookies(h)
 #'
-#' # See http://www.cookiecentral.com/faq/#3.5
+#' # Server sets cookies
+#' req <- curl_perform("http://httpbin.org/cookies/set?foo=123&bar=ftw", handle = h)
+#' get_handle_cookies(h)
+#'
+#' # Server deletes cookies
+#' req <- curl_perform("http://httpbin.org/cookies/delete?foo", handle = h)
 #' get_handle_cookies(h)
 get_handle_cookies <- function(handle){
   cookies <- .Call(R_get_handle_cookies, handle)
   df <- if(length(cookies)){
-    as.data.frame(do.call(rbind, strsplit(cookies, split="\t")))
+    values <- lapply(strsplit(cookies, split="\t"), `[`, 1:7)
+    as.data.frame(do.call(rbind, values), stringsAsFactors = FALSE)
   } else {
     as.data.frame(matrix(ncol=7, nrow=0))
   }
   names(df) <- c("domain", "flag", "path", "secure", "expiration", "name", "value")
   df$flag <- as.logical(df$flag)
   df$secure <- as.logical(df$secure)
+  df$expiration <- as.numeric(df$expiration)
   df
 }
