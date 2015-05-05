1e9fa26b7eec4184e316e354dd33d1beba7b9fa9
Jeroen Ooms <jeroenooms@gmail.com>
2015-05-05 10:59:14 -0700

    Trigger CI test

---
+++ src/base64.c
@@ -0,0 +1,57 @@
+/* *
+ * Derived from: https://gist.github.com/barrysteyn/7308212
+ * License: MIT
+ */
+
+#include "apple.h"
+#include <openssl/bio.h>
+#include <openssl/evp.h>
+#include <openssl/buffer.h>
+#include <string.h>
+#include <assert.h>
+
+size_t calcDecodeLength(const char* b64input) { //Calculates the length of a decoded string
+  size_t len = strlen(b64input);
+  size_t padding = 0;
+
+  if (b64input[len-1] == '=' && b64input[len-2] == '=') //last two chars are =
+    padding = 2;
+  else if (b64input[len-1] == '=') //last char is =
+    padding = 1;
+
+  return (len*3)/4 - padding;
+}
+
+void Base64Decode(char* b64message, uint8_t** buffer, size_t* length) { //Decodes a base64 encoded string
+  BIO *bio, *b64;
+
+  int decodeLen = calcDecodeLength(b64message);
+  *buffer = (uint8_t*)malloc(decodeLen);
+
+  bio = BIO_new_mem_buf(b64message, -1);
+  b64 = BIO_new(BIO_f_base64());
+  bio = BIO_push(b64, bio);
+
+  BIO_set_flags(bio, BIO_FLAGS_BASE64_NO_NL); //Do not use newlines to flush buffer
+  *length = BIO_read(bio, *buffer, strlen(b64message));
+  assert(*length == decodeLen); //length should equal decodeLen, else something went horribly wrong
+  BIO_free_all(bio);
+}
+
+void Base64Encode(const uint8_t* buffer, size_t length, char** b64text) { //Encodes a binary safe base 64 string
+  BIO *bio, *b64;
+  BUF_MEM *bufferPtr;
+
+  b64 = BIO_new(BIO_f_base64());
+  bio = BIO_new(BIO_s_mem());
+  bio = BIO_push(b64, bio);
+
+  BIO_set_flags(bio, BIO_FLAGS_BASE64_NO_NL); //Ignore newlines - write everything in one line
+  BIO_write(bio, buffer, length);
+  BIO_flush(bio);
+  BIO_get_mem_ptr(bio, &bufferPtr);
+  BIO_set_close(bio, BIO_NOCLOSE);
+  BIO_free_all(bio);
+
+  *b64text=(*bufferPtr).data;
+}
