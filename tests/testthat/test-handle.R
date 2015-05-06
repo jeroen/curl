dc3d7efc3f823a501fdaeb10ccfb6aacb3d2da65
Jeroen Ooms <jeroenooms@gmail.com>
2015-05-06 15:50:45 -0700

    Rename curl_perform to curl_fetch_memory in unit tests.

--- tests/testthat/test-handle.R
+++ tests/testthat/test-handle.R
@@ -3,29 +3,29 @@ context("Reusable handle")
 h <- new_handle()
 
 test_that("Perform", {
-  expect_equal(curl_perform("http://httpbin.org/get", handle = h)$status, 200)
-  expect_equal(curl_perform("http://httpbin.org/cookies", handle = h)$status, 200)
-  expect_equal(curl_perform("http://httpbin.org/status/418", handle = h)$status, 418)
+  expect_equal(curl_fetch_memory("http://httpbin.org/get", handle = h)$status, 200)
+  expect_equal(curl_fetch_memory("http://httpbin.org/cookies", handle = h)$status, 200)
+  expect_equal(curl_fetch_memory("http://httpbin.org/status/418", handle = h)$status, 418)
 })
 
 test_that("Redirect", {
-  expect_equal(curl_perform("http://httpbin.org/redirect/6", handle = h)$status, 200)
-  expect_equal(curl_perform("http://httpbin.org/relative-redirect/6", handle = h)$status, 200)
-  expect_equal(curl_perform("http://httpbin.org/absolute-redirect/6", handle = h)$status, 200)
+  expect_equal(curl_fetch_memory("http://httpbin.org/redirect/6", handle = h)$status, 200)
+  expect_equal(curl_fetch_memory("http://httpbin.org/relative-redirect/6", handle = h)$status, 200)
+  expect_equal(curl_fetch_memory("http://httpbin.org/absolute-redirect/6", handle = h)$status, 200)
 })
 
 test_that("Cookies", {
-  expect_equal(curl_perform("http://httpbin.org/cookies/set?foo=123&bar=456", handle = h)$status, 200)
-  expect_equal(jsonlite::fromJSON(rawToChar(curl_perform("http://httpbin.org/cookies", handle = h)$content))$cookies$bar, "456")
-  expect_equal(curl_perform("http://httpbin.org/cookies/delete?bar", handle = h)$status, 200)
-  expect_equal(jsonlite::fromJSON(rawToChar(curl_perform("http://httpbin.org/cookies", handle = h)$content))$cookies$bar, NULL)
+  expect_equal(curl_fetch_memory("http://httpbin.org/cookies/set?foo=123&bar=456", handle = h)$status, 200)
+  expect_equal(jsonlite::fromJSON(rawToChar(curl_fetch_memory("http://httpbin.org/cookies", handle = h)$content))$cookies$bar, "456")
+  expect_equal(curl_fetch_memory("http://httpbin.org/cookies/delete?bar", handle = h)$status, 200)
+  expect_equal(jsonlite::fromJSON(rawToChar(curl_fetch_memory("http://httpbin.org/cookies", handle = h)$content))$cookies$bar, NULL)
 })
 
 test_that("Compression", {
   expect_equal(jsonlite::fromJSON(readLines(curl("http://httpbin.org/deflate", handle = h)))$deflate, TRUE)
-  expect_equal(jsonlite::fromJSON(rawToChar(curl_perform("http://httpbin.org/deflate", handle = h)$content))$deflate, TRUE)
+  expect_equal(jsonlite::fromJSON(rawToChar(curl_fetch_memory("http://httpbin.org/deflate", handle = h)$content))$deflate, TRUE)
   expect_equal(jsonlite::fromJSON(readLines(curl("http://httpbin.org/gzip", handle = h)))$gzipped, TRUE)
-  expect_equal(jsonlite::fromJSON(rawToChar(curl_perform("http://httpbin.org/gzip", handle = h)$content))$gzipped, TRUE)
+  expect_equal(jsonlite::fromJSON(rawToChar(curl_fetch_memory("http://httpbin.org/gzip", handle = h)$content))$gzipped, TRUE)
   suppressWarnings(gc())
 })
 
@@ -40,33 +40,33 @@ test_that("Opening and closing a connection",{
   con <- curl("http://httpbin.org/cookies", handle = h)
 
   # Handle is still usable
-  expect_equal(curl_perform("http://httpbin.org/get", handle = h)$status, 200)
+  expect_equal(curl_fetch_memory("http://httpbin.org/get", handle = h)$status, 200)
 
   # Opening the connection locks the handle
   open(con)
 
   # Recent versions of libcurl will raise an error
   #if(compareVersion(curl_version()$version, "7.37") > 0){
-  #  expect_error(curl_perform("http://httpbin.org/get", handle = h))
+  #  expect_error(curl_fetch_memory("http://httpbin.org/get", handle = h))
   #}
 
   expect_equal(jsonlite::fromJSON(readLines(con))$cookies$foo, "123")
 
   # After closing it is free again
   close(con)
-  expect_equal(curl_perform("http://httpbin.org/get", handle = h)$status, 200)
+  expect_equal(curl_fetch_memory("http://httpbin.org/get", handle = h)$status, 200)
 
   # Removing the connection also unlocks the handle
   con <- curl("http://httpbin.org/cookies", "rb", handle = h)
 
   # Recent versions of libcurl will raise an error
   #if(compareVersion(curl_version()$version, "7.37") > 0){
-  #  expect_error(curl_perform("http://httpbin.org/get", handle = h))
+  #  expect_error(curl_fetch_memory("http://httpbin.org/get", handle = h))
   #}
 
   rm(con)
   suppressWarnings(gc())
-  expect_equal(curl_perform("http://httpbin.org/get", handle = h)$status, 200)
+  expect_equal(curl_fetch_memory("http://httpbin.org/get", handle = h)$status, 200)
 })
 
 test_that("Downloading to a file", {
