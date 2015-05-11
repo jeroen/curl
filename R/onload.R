baa52723360228b255f8e4ec050db11b402a2368
hadley <h.wickham@gmail.com>
2015-05-11 11:22:42 -0500

    Style tweaks to .onLoad
    Switch from message to warning to avoid R CMD check NOTE, and because this is a failure mode. (packageStartupMessage can only be used in .onAttach)

--- R/onload.R
+++ R/onload.R
@@ -1,16 +1,18 @@
 .onLoad <- function(libname, pkgname){
+  if (!grepl("mingw", R.Version()$platform))
+    return()
+
   # Enable SSL on Windows if CA bundle is available (R >= 3.2.0)
-  if(grepl("mingw", R.Version()$platform)){
-    bundle <- Sys.getenv("CURL_CA_BUNDLE", file.path(Sys.getenv("R_Home"), "etc/curl-ca-bundle.crt"))
-    if(file.exists(bundle)){
-      set_bundle(bundle)
-    } else {
-      packageStartupMessage("No CA bundle found. SSL validation disabled.")
-    }
+  bundle <- Sys.getenv("CURL_CA_BUNDLE",
+    file.path(R.home("etc"), "etc/curl-ca-bundle.crt"))
+  if (bundle != "" && file.exists(bundle)) {
+    set_bundle(bundle)
+  } else {
+    warning("No CA bundle found. SSL validation disabled.", call. = FALSE)
   }
 }
 
 #' @useDynLib curl R_set_bundle
 set_bundle <- function(path){
-  .Call(R_set_bundle, path);
+  .Call(R_set_bundle, path)
 }
