70933be6f824f9d58c64be1e519a288b62ac85ca
Jeroen Ooms <jeroenooms@gmail.com>
2015-07-04 11:09:27 +0200

    Verify host instead of peer

--- tests/testthat/test-certificates.R
+++ tests/testthat/test-certificates.R
@@ -5,6 +5,6 @@ test_that("Perform", {
     expect_equal(curl_fetch_memory("https://httpbin.org/get")$status, 200)
     fake_url <- paste0("https://", utils::nsl("httpbin.org"), "/get")
     expect_error(curl_fetch_memory(fake_url), "certificate")
-    expect_equal(curl_fetch_memory(fake_url, handle = new_handle(ssl_verifypeer = FALSE))$status, 200)
+    expect_equal(curl_fetch_memory(fake_url, handle = new_handle(ssl_verifyhost = FALSE))$status, 200)
   }
 })
