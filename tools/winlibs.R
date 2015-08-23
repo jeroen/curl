e62ebfd9d01bd37049bf3fda51697219ccf865df
Jeroen Ooms <jeroenooms@gmail.com>
2015-08-23 21:19:38 +0200

    Version fix

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,6 +1,6 @@
 # Build against static libraries from curl website.
 if(!file.exists("../windows/libcurl-7.43.0/include/curl/curl.h")){
-  if(getRversion() <= "3.2.2") setInternet2()
+  if(getRversion() < "3.3.0") setInternet2()
   download.file("https://github.com/rwinlib/libcurl/archive/v7.43.0.zip", "lib.zip", quiet = TRUE)
   dir.create("../windows", showWarnings = FALSE)
   unzip("lib.zip", exdir = "../windows")
