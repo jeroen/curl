5f85a7f7a881be7497afe937f025bb12b54f5fd3
Jeroen Ooms <jeroenooms@gmail.com>
2016-01-23 14:43:01 +0100

    More NSlookup

--- R/nslooup.R
+++ R/nslooup.R
@@ -3,9 +3,13 @@
 #' The same thing as \link{nsl} but supports IPv6 and works on all platforms.
 #'
 #' @export
-#' @param hostname a string with a host
+#' @param hostname a string with a hostname
 #' @useDynLib curl R_nslookup
+#' @examples nslookup("www.r-project.org")
+#' nslookup("ipv6.test-ipv6.com")
 nslookup <- function(host){
   stopifnot(is.character(host))
+  if(grepl("://", host, fixed = TRUE))
+    stop("This looks like a URL, not a hostname")
   .Call(R_nslookup, host[1])
 }
