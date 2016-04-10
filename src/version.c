32c7bee630add3c915d0d10b64911bffc630e1d9
Jeroen Ooms <jeroenooms@gmail.com>
2016-04-10 05:32:03 +0200

    Add some additional version numbers

--- src/version.c
+++ src/version.c
@@ -8,11 +8,13 @@ SEXP R_curl_version() {
   const curl_version_info_data *data = curl_version_info(CURLVERSION_NOW);
 
   /* put stuff in a list */
-  SEXP list = PROTECT(allocVector(VECSXP, 5));
+  SEXP list = PROTECT(allocVector(VECSXP, 7));
   SET_VECTOR_ELT(list, 0, make_string(data->version));
   SET_VECTOR_ELT(list, 1, make_string(data->ssl_version));
   SET_VECTOR_ELT(list, 2, make_string(data->libz_version));
-  SET_VECTOR_ELT(list, 3, make_string(data->host));
+  SET_VECTOR_ELT(list, 3, make_string(data->libssh_version));
+  SET_VECTOR_ELT(list, 4, make_string(data->libidn));
+  SET_VECTOR_ELT(list, 5, make_string(data->host));
 
   /* create vector of protocols */
   const char *const * temp = data->protocols;
@@ -22,15 +24,17 @@ SEXP R_curl_version() {
   for (int i = 0; i < len; i++){
     SET_STRING_ELT(protocols, i, mkChar(*(data->protocols + i)));
   }
-  SET_VECTOR_ELT(list, 4, protocols);
+  SET_VECTOR_ELT(list, 6, protocols);
 
   /* add list names */
-  SEXP names = PROTECT(allocVector(STRSXP, 5));
+  SEXP names = PROTECT(allocVector(STRSXP, 7));
   SET_STRING_ELT(names, 0, mkChar("version"));
   SET_STRING_ELT(names, 1, mkChar("ssl_version"));
   SET_STRING_ELT(names, 2, mkChar("libz_version"));
-  SET_STRING_ELT(names, 3, mkChar("host"));
-  SET_STRING_ELT(names, 4, mkChar("protocols"));
+  SET_STRING_ELT(names, 3, mkChar("libssh_version"));
+  SET_STRING_ELT(names, 4, mkChar("libidn"));
+  SET_STRING_ELT(names, 5, mkChar("host"));
+  SET_STRING_ELT(names, 6, mkChar("protocols"));
   setAttrib(list, R_NamesSymbol, names);
 
   /* return */
