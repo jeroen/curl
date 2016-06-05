a841ef07ed28f5ac25494cfd441e42ebe4bf723a
Jeroen Ooms <jeroenooms@gmail.com>
2016-06-05 22:01:29 +0200

    Skip test on old libcurl

--- tests/testthat/test-multi.R
+++ tests/testthat/test-multi.R
@@ -1,15 +1,16 @@
 context("Multi handle")
 
 test_that("Max connections works", {
+  skipifnot(curl_version()$version >= as.numeric_version("7.30"))
   for(i in 1:3){
     multi_add(new_handle(url = "https://httpbin.org/delay/3"))
   }
   out <- multi_run(host_connections = 1, timeout = 5)
-  expect_equal(out, list(success=1, error=0, pending=2))
+  expect_equal(out, list(success = 1, error = 0, pending = 2))
   out <- multi_run(3, host_connections = 1)
-  expect_equal(out, list(success=1, error=0, pending=1))
+  expect_equal(out, list(success = 1, error = 0, pending = 1))
   out <- multi_run(host_connections = 1)
-  expect_equal(out, list(success=1, error=0, pending=0))
+  expect_equal(out, list(success = 1, error = 0, pending = 0))
 })
 
 test_that("Max connections reset", {
@@ -17,7 +18,7 @@ test_that("Max connections reset", {
     multi_add(new_handle(url = "https://httpbin.org/delay/3"))
   }
   out <- multi_run(host_connections = 6, timeout = 5)
-  expect_equal(out, list(success=3, error=0, pending=0))
+  expect_equal(out, list(success = 3, error = 0, pending = 0))
 })
 
 test_that("Timeout works", {
