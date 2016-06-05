420f0f86d12f95b918252c700c14bbe02f295f9c
Jeroen Ooms <jeroenooms@gmail.com>
2016-06-05 23:56:35 +0200

    Documentation

--- R/multi.R
+++ R/multi.R
@@ -3,6 +3,14 @@
 #' AJAX style concurrent requests, possibly using HTTP/2 multiplexing.
 #' Results are only available via callback functions. Advanced use only!
 #'
+#' Requests are created in the usual way using a curl \link{handle} and added
+#' to the scheduler with \link{multi_add}. This function returns immediately
+#' and does not perform the request yet. After zero or more handles have been
+#' added, the \code{multi_run} function performs all requests concurrently.
+#' It returns when all requests have completed or when \code{timeout} has
+#' passed, or when the user interrupts by pressing \code{ESC} or \code{CTRL+C}.
+#' The \code{multi_run} function can be called to resume requests.
+#'
 #' A single handle cannot be used for multiple simultaneous requests. However
 #' it is possible to add new requests to a pool while it is running, so you
 #' can re-use a handle within the callback of a request from that same handle.
@@ -23,10 +31,10 @@
 #' multi_add(h2, function(res){print(res)})
 #' multi_add(h3)
 #' multi_run(timeout = 3)
-multi_add <- function(handle, complete = identity, error = identity){
+multi_add <- function(handle, complete = NULL, error = NULL){
   stopifnot(inherits(handle, "curl_handle"))
-  stopifnot(is.function(complete))
-  stopifnot(is.function(error))
+  stopifnot(is.null(complete) || is.function(complete))
+  stopifnot(is.null(error) || is.function(error))
   .Call(R_multi_add, handle, complete, error)
 }
 
