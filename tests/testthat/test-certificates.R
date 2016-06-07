8ba1df136d2ce85cd95ff88ae5b256b2e41ab40b
Jeroen Ooms <jeroenooms@gmail.com>
2016-06-07 22:30:13 +0200

    Use 'nslookup' instead of utils::nsl

--- tests/testthat/test-certificates.R
+++ tests/testthat/test-certificates.R
@@ -3,7 +3,7 @@ context("Certificate validation")
 test_that("Perform", {
   if (!grepl("mingw", R.Version()$platform)) {
     expect_equal(curl_fetch_memory("https://httpbin.org/get")$status, 200)
-    fake_url <- paste0("https://", utils::nsl("httpbin.org"), "/get")
+    fake_url <- paste0("https://", nslookup("httpbin.org"), "/get")
     expect_error(curl_fetch_memory(fake_url), "certificate")
     expect_equal(curl_fetch_memory(fake_url, handle = new_handle(ssl_verifyhost = FALSE))$status, 200)
   }
