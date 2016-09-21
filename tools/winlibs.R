f73b590a8332afb56489bc3c9f01d340a5bfb69b
Jeroen Ooms <jeroenooms@gmail.com>
2016-09-21 13:43:30 +0200

    Upgrade libcurl on Windows

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,7 +1,7 @@
 # Build against static libraries from curl website.
-if(!file.exists("../windows/libcurl-7.47.1/include/curl/curl.h")){
+if(!file.exists("../windows/libcurl-7.50.3/include/curl/curl.h")){
   if(getRversion() < "3.3.0") setInternet2()
-  download.file("https://github.com/rwinlib/libcurl/archive/v7.47.1.zip", "lib.zip", quiet = TRUE)
+  download.file("https://github.com/rwinlib/libcurl/archive/v7.50.3.zip", "lib.zip", quiet = TRUE)
   dir.create("../windows", showWarnings = FALSE)
   unzip("lib.zip", exdir = "../windows")
   unlink("lib.zip")
