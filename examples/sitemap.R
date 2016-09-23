1faf1436e68e017126653d08520474f6cca2ba4f
Jeroen Ooms <jeroenooms@gmail.com>
2016-09-23 14:32:10 +0200

    Tweak example

--- examples/sitemap.R
+++ examples/sitemap.R
@@ -16,28 +16,35 @@ get_links <- function(res){
     links <- xml2:::url_absolute(links, res$url)
     links <- grep("^https?://", links, value = TRUE)
     links <- sub("#.*", "", links)
-    links <- sub("index.html$", "", links)
+    links <- sub("/index.html$", "/", links)
     unique(sub("/$", "", links))
   }, error = function(e){character()})
 }
 
 crawl <- function(root, timeout = 300){
+  total_visited = 0
   pages <- new.env()
-  pool <- curl::new_pool(total_con = 100, host_con = 100)
+  pool <- curl::new_pool(total_con = 500, host_con = 100)
 
   crawl_page <- function(url){
     pages[[url]] <- NA
     h <- curl::new_handle(failonerror = TRUE, nobody = TRUE)
     curl_fetch_multi(url, handle = h, pool = pool, done = function(res){
+      total_visited <<- total_visited + 1
+      if(!identical(url, res$url)){
+        pages[[url]] = res$url
+        url = res$url
+      }
       headers <- curl::parse_headers(res$headers)
       ctype <- headers[grepl("^content-type", headers, ignore.case = TRUE)]
-      cat(sprintf("Found a %s file at: %s\n", ctype, url))
+      cat(sprintf("[%d] Found '%s' @ %s\n", total_visited, ctype, url))
       if(isTRUE(grepl("text/html", ctype))){
         handle_setopt(h, nobody = FALSE, maxfilesize = 1e6)
         curl::curl_fetch_multi(url, handle = h, pool = pool, done = function(res){
+          total_visited <<- total_visited + 1
           links = get_links(res)
-          cat(sprintf("Done (%d): %s (%d links)\n", length(pages), url, length(links)))
-          links <- grep(paste0("^", root), links, value = TRUE)
+          cat(sprintf("[%d] Extracted %d hyperlinks from %s\n", total_visited, length(links), url))
+          links <- grep(root, links, value = TRUE, fixed = TRUE)
           pages[[url]] <- links
           lapply(links, function(href){
             if(is.null(pages[[href]]))
@@ -56,4 +63,4 @@ crawl <- function(root, timeout = 300){
   as.list(pages)
 }
 
-sitemap <- crawl(root = 'https://cloud.r-project.org')
+sitemap <- crawl(root = 'https://cloud.r-project.org/web/packages', timeout = Inf)
