d0a21e618eb07de6e309466df856eaf5354fb085
Jeroen Ooms <jeroenooms@gmail.com>
2016-09-25 00:51:25 +0200

    Fix

--- src/multi.c
+++ src/multi.c
@@ -223,7 +223,7 @@ SEXP R_multi_setopt(SEXP pool_ptr, SEXP total_con, SEXP host_con, SEXP multiplex
   #ifdef CURLPIPE_MULTIPLEX
     massert(curl_multi_setopt(multi, CURLMOPT_PIPELINING, CURLPIPE_HTTP1 & CURLPIPE_MULTIPLEX));
   #else
-    massert(curl_multi_setopt(multi, CURLMOPT_PIPELINING, CURLPIPE_HTTP1));
+    massert(curl_multi_setopt(multi, CURLMOPT_PIPELINING, (long) 1));
   #endif
 
   #ifdef HAS_CURLMOPT_MAX_TOTAL_CONNECTIONS
