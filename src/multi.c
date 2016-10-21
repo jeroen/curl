bdc5d1a7c4db592a415db2f164481e760ebfc60c
Jeroen Ooms <jeroenooms@gmail.com>
2016-10-21 12:53:19 +0200

    Fix multiplex disabling

--- src/multi.c
+++ src/multi.c
@@ -219,11 +219,11 @@ SEXP R_multi_new(){
 SEXP R_multi_setopt(SEXP pool_ptr, SEXP total_con, SEXP host_con, SEXP multiplex){
   multiref *mref = get_multiref(pool_ptr);
   CURLM *multi = mref->m;
-  if(asLogical(multiplex))
 
   // NOTE: CURLPIPE_HTTP1 is unsafe for non idempotent requests
   #ifdef CURLPIPE_MULTIPLEX
-    massert(curl_multi_setopt(multi, CURLMOPT_PIPELINING, CURLPIPE_MULTIPLEX));
+    massert(curl_multi_setopt(multi, CURLMOPT_PIPELINING,
+                              asLogical(multiplex) ? CURLPIPE_MULTIPLEX : CURLPIPE_NOTHING));
   #endif
 
   #ifdef HAS_CURLMOPT_MAX_TOTAL_CONNECTIONS
