1fe5e797efb1a2fe55b3e75aa74024920bad71d1
Jeroen Ooms <jeroenooms@gmail.com>
2016-11-12 21:27:53 +0100

    use dynamic mirror select

--- tests/testthat/helper-version.R
+++ tests/testthat/helper-version.R
@@ -1,12 +1,34 @@
-options(curl_interrupt = TRUE)
 cat("This is libcurl version", curl_version()$version, "with", curl_version()$ssl_version, "\n")
 
 # Try to load test server
-testserver <- "https://http2bin.org"
-req <- curl_fetch_memory("http://jeroenooms.github.io/curl/httpbin")
-if(req$status == 200){
-  testserver <- paste0(sub("[ \t\r\n]+", "", rawToChar(req$content)), "/")
+find_test_server <- function(){
+  h <- curl::new_handle(timeout = 10, failonerror = TRUE)
+
+  # Try to download latest test-server list
+  servers <- tryCatch({
+    req <- curl_fetch_memory("http://jeroenooms.github.io/curl/servers", handle = h)
+    strsplit(rawToChar(req$content), "\n", fixed = TRUE)[[1]]
+  }, error = function(e){
+    message("Failed to download server list:", e$message)
+    c("https://httpbin.org", "https://eu.httpbin.org", "http://httpbin.org", "https://http2bin.org")
+  })
+
+
+  # Try each test-server in the list
+  for(host in servers){
+    tryCatch({
+      url <- paste0(host, "/get")
+      req <- curl_fetch_memory(url, handle = h)
+      return(host)
+    }, error = function(e){
+      message(paste0("Not using ", host, ": ", e$message))
+    })
+  }
+
+  stop("All testing servers seem unavailable. No internet connection?")
 }
+
+testserver <- find_test_server()
 cat("Using test server:", testserver, "\n")
 
 httpbin <- function(path){
