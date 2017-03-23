5d7b6dff781d88b0221a3fffb97809fd5026d91d
Jeroen Ooms <jeroenooms@gmail.com>
2017-03-23 13:37:36 +0100

    Update libcurl on Windows

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,7 +1,7 @@
 # Build against static libraries from rwinlib
-if(!file.exists("../windows/libcurl-7.51.0/include/curl/curl.h")){
+if(!file.exists("../windows/libcurl-7.53.1/include/curl/curl.h")){
   if(getRversion() < "3.3.0") setInternet2()
-  download.file("https://github.com/rwinlib/libcurl/archive/v7.51.0.zip", "lib.zip", quiet = TRUE)
+  download.file("https://github.com/rwinlib/libcurl/archive/v7.53.1.zip", "lib.zip", quiet = TRUE)
   dir.create("../windows", showWarnings = FALSE)
   unzip("lib.zip", exdir = "../windows")
   unlink("lib.zip")
