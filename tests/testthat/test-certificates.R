cbf68c3172f43df42f9e8f79d305abce6617cf9f
Jeroen Ooms <jeroenooms@gmail.com>
2017-04-27 18:20:49 +0200

    Fix unit test on Mavericks

--- tests/testthat/test-certificates.R
+++ tests/testthat/test-certificates.R
@@ -1,10 +1,7 @@
 context("Certificate validation")
 
 test_that("Invalid domain raises an error", {
-  ipaddr <- nslookup("www.google.com")
-  #fix for ipv6 DNS
-  if(grepl(":", ipaddr))
-    ipaddr <- sprintf("[%s]", ipaddr)
+  ipaddr <- nslookup("www.google.com", ipv4_only = TRUE)
   fake_url <- paste0("https://", ipaddr)
   expect_error(curl_fetch_memory(fake_url), "certificate")
   expect_is(curl_fetch_memory(fake_url, handle = new_handle(ssl_verifyhost = FALSE))$status, "integer")
