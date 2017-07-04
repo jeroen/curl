8545f7bd6db3575e9a93ac67d8ada27b7a2cf087
Jeroen Ooms <jeroenooms@gmail.com>
2017-07-04 06:34:36 -0700

    Use new libcurl builds on Windows

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,7 +1,7 @@
 # Build against static libraries from rwinlib
-if(!file.exists("../windows/libcurl-7.53.1/include/curl/curl.h")){
+if(!file.exists("../windows/libcurl-7.54.1/include/curl/curl.h")){
   if(getRversion() < "3.3.0") setInternet2()
-  download.file("https://github.com/rwinlib/libcurl/archive/v7.53.1.zip", "lib.zip", quiet = TRUE)
+  download.file("https://github.com/rwinlib/libcurl/archive/v7.54.1.zip", "lib.zip", quiet = TRUE)
   dir.create("../windows", showWarnings = FALSE)
   unzip("lib.zip", exdir = "../windows")
   unlink("lib.zip")
