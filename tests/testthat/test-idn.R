7a1a322992efc728244880eb82459de740e74139
Jeroen Ooms <jeroenooms@gmail.com>
2017-07-06 08:25:14 -0700

    A few simple IDN unit tests

---
+++ tests/testthat/test-idn.R
@@ -0,0 +1,14 @@
+context("IDN")
+
+test_that("IDN domain names",{
+  malmo <- "http://www.malm\u00F6.se"
+  expect_is(curl::curl_fetch_memory(enc2utf8(malmo))$status_code, "integer")
+  expect_is(curl::curl_fetch_memory(enc2native(malmo))$status_code, "integer")
+
+  kremlin <- "http://\u043F\u0440\u0435\u0437\u0438\u0434\u0435\u043D\u0442.\u0440\u0444"
+  expect_is(curl::curl_fetch_memory(enc2utf8(kremlin))$status_code, "integer")
+
+  # Something random that doesn't exist
+  wrong <- "http://\u043F\u0840\u0435\u0537\u0438\u0433\u0435\u043F\u0442.\u0440\u0444"
+  expect_error(curl::curl_fetch_memory(enc2utf8(wrong)), 'resolve')
+})
