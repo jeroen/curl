f2a5119b03b7d21b246e7dcf787826d03fae3852
Jeroen <jeroenooms@gmail.com>
2017-07-08 21:39:40 +0200

    Add IDN to version

--- src/version.c
+++ src/version.c
@@ -8,7 +8,7 @@ SEXP R_curl_version() {
   const curl_version_info_data *data = curl_version_info(CURLVERSION_NOW);
 
   /* put stuff in a list */
-  SEXP list = PROTECT(allocVector(VECSXP, 9));
+  SEXP list = PROTECT(allocVector(VECSXP, 10));
   SET_VECTOR_ELT(list, 0, make_string(data->version));
   SET_VECTOR_ELT(list, 1, make_string(data->ssl_version));
   SET_VECTOR_ELT(list, 2, make_string(data->libz_version));
@@ -27,7 +27,7 @@ SEXP R_curl_version() {
   SET_VECTOR_ELT(list, 6, protocols);
 
   /* add list names */
-  SEXP names = PROTECT(allocVector(STRSXP, 9));
+  SEXP names = PROTECT(allocVector(STRSXP, 10));
   SET_STRING_ELT(names, 0, mkChar("version"));
   SET_STRING_ELT(names, 1, mkChar("ssl_version"));
   SET_STRING_ELT(names, 2, mkChar("libz_version"));
@@ -37,6 +37,7 @@ SEXP R_curl_version() {
   SET_STRING_ELT(names, 6, mkChar("protocols"));
   SET_STRING_ELT(names, 7, mkChar("ipv6"));
   SET_STRING_ELT(names, 8, mkChar("http2"));
+  SET_STRING_ELT(names, 9, mkChar("idn"));
   setAttrib(list, R_NamesSymbol, names);
 
   #ifdef CURL_VERSION_IPV6
@@ -49,6 +50,12 @@ SEXP R_curl_version() {
   SET_VECTOR_ELT(list, 8, ScalarLogical(data->features & CURL_VERSION_HTTP2));
   #else
   SET_VECTOR_ELT(list, 8, ScalarLogical(0));
+  #endif
+
+  #ifdef CURL_VERSION_IDN
+    SET_VECTOR_ELT(list, 9, ScalarLogical(data->features & CURL_VERSION_IDN));
+  #else
+    SET_VECTOR_ELT(list, 9, ScalarLogical(0));
   #endif
 
   /* return */
