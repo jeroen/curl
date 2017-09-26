fbe411a7b12384409256792c0ad55a0d4562624d
Jeroen <jeroenooms@gmail.com>
2017-09-26 12:50:41 +0200

    Further improve multi examples

--- R/multi.R
+++ R/multi.R
@@ -52,18 +52,25 @@
 #' @param pool a multi handle created by \link{new_pool}. Default uses a global pool.
 #' @export
 #' @examples
+#' results <- list()
+#' success <- function(x){
+#'   results <<- append(results, list(x))
+#' }
+#' failure <- function(str){
+#'   cat(paste("Failed request:", str), file = stderr())
+#' }
 #' # This handle will take longest (3sec)
 #' h1 <- new_handle(url = "https://eu.httpbin.org/delay/3")
-#' multi_add(h1, done = print, fail = print)
+#' multi_add(h1, done = success, fail = failure)
 #'
 #' # This handle writes data to a file
 #' con <- file("output.txt", open = "wb")
 #' h2 <- new_handle(url = "https://eu.httpbin.org/post", postfields = "bla bla")
-#' multi_add(h2, done = print, fail = print, data = con)
+#' multi_add(h2, done = success, fail = failure, data = con)
 #'
 #' # This handle raises an error
 #' h3 <- new_handle(url = "https://urldoesnotexist.xyz")
-#' multi_add(h3, done = print, fail = print)
+#' multi_add(h3, done = success, fail = failure)
 #'
 #' # Actually perform the requests
 #' multi_run(timeout = 2)
