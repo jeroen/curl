8bf934a6d88b870848d270c1dd2293018bbd9992
Jeroen Ooms <jeroenooms@gmail.com>
2018-10-04 23:37:20 +0200

    Fix unit test for openssl 1.1.1

--- tests/testthat/test-certificates.R
+++ tests/testthat/test-certificates.R
@@ -13,7 +13,8 @@ test_that("Invalid domain raises an error", {
   ipaddr <- nslookup("www.google.com", ipv4_only = TRUE)
   fake_url <- paste0("https://", ipaddr)
   expect_error(curl_fetch_memory(fake_url), "certificate")
-  expect_is(curl_fetch_memory(fake_url, handle = new_handle(ssl_verifyhost = FALSE))$status, "integer")
+  expect_is(curl_fetch_memory(fake_url, handle = new_handle(
+    ssl_verifyhost = FALSE, ssl_verifypeer = FALSE))$status, "integer")
 })
 
 test_that("GC works", {
