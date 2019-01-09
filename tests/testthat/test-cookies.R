b64178d281e377fad08ef9bd50c6f3230029722e
Jeroen Ooms <jeroenooms@gmail.com>
2019-01-09 15:43:53 +0100

    Fix unit test for curl 7.63+

--- tests/testthat/test-cookies.R
+++ tests/testthat/test-cookies.R
@@ -28,10 +28,13 @@ test_that("Delete a cookie", {
   cookies <- handle_cookies(h)
   foo <- subset(cookies, name == "foo")
   bar <- subset(cookies, name == "bar")
-  expect_true(foo$expiration < Sys.time())
   expect_true(bar$expiration > Sys.time())
-  expect_true(is.na(foo$value))
   expect_equal(bar$value, "ftw")
+
+  # TODO: remove this when fixed upstream
+  skip_if(curl_version()$version >= "7.63.0")
+  expect_true(foo$expiration < Sys.time())
+  expect_true(is.na(foo$value))
 })
 
 test_that("Overwrite a cookie", {
