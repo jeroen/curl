d6c3447be47212cda856622151b8754569a8f346
Jeroen Ooms <jeroenooms@gmail.com>
2019-08-19 18:58:25 +0200

    Windows: try to respect CURL_SSL_BACKEND

--- src/ssl.c
+++ src/ssl.c
@@ -7,7 +7,9 @@
 
 int windows_openssl = 0;
 
-/* Fall back on OpenSSL on Legacy Windows (Vista/2008) which do not support TLS 1.2 natively */
+/* Force OpenSSL on Legacy Windows (Vista/2008) which do not support TLS 1.2 natively.
+ * On other systems we let libcurl choose so you can set the 'CURL_SSL_BACKEND' variable.
+ */
 void select_ssl_backend(){
 #if defined(_WIN32) && defined(HAS_MULTI_SSL)
   DWORD dwBuild = 0;
@@ -16,23 +18,21 @@ void select_ssl_backend(){
     dwBuild = (DWORD)(HIWORD(dwVersion));
 
   /* TLS 1.2 requires at least Windows 7 or 2008-R2 */
-  curl_sslbackend backend = dwBuild < 7600 ? CURLSSLBACKEND_OPENSSL : CURLSSLBACKEND_SCHANNEL;
-
-  /* Try to set the backend */
-  switch(curl_global_sslset(backend, NULL, NULL)){
-  case CURLSSLSET_OK :
-    if(backend == CURLSSLBACKEND_OPENSSL)
+  if(dwBuild < 7600){
+    switch(curl_global_sslset(CURLSSLBACKEND_OPENSSL, NULL, NULL)){
+    case CURLSSLSET_OK :
       windows_openssl = 1;
-    break;
-  case CURLSSLSET_TOO_LATE:
-    Rf_warning("Failed to set libcurl SSL: already initiated");
-    break;
-  case CURLSSLSET_UNKNOWN_BACKEND:
-    Rf_warning("Failed to set libcurl SSL: unsupported backend");
-    break;
-  default:
-    Rf_warning("Failed to set libcurl SSL: unknown error");
-    break;
+      break;
+    case CURLSSLSET_TOO_LATE:
+      Rf_warning("Failed to set libcurl SSL: already initiated");
+      break;
+    case CURLSSLSET_UNKNOWN_BACKEND:
+      Rf_warning("Failed to set libcurl SSL: unsupported backend");
+      break;
+    default:
+      Rf_warning("Failed to set libcurl SSL: unknown error");
+      break;
+    }
   }
 
 #endif
