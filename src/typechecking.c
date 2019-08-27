ca7ee35342bd693b2321abba9f1d2222bdd05521
Jeroen Ooms <jeroenooms@gmail.com>
2019-08-27 20:29:07 +0200

    Fixed typechecking code for new internal macro names in libcurl 7.66

--- src/typechecking.c
+++ src/typechecking.c
@@ -1,6 +1,7 @@
-/* Hack to get the GCC macros on all systems */
 #include <curl/curl.h>
-#ifndef __CURL_TYPECHECK_GCC_H
+
+/* Hack to get the GCC macros on other systems */
+#if !defined(__CURL_TYPECHECK_GCC_H) || !defined(CURLINC_TYPECHECK_GCC_H)
 #undef curl_easy_setopt
 #undef curl_easy_getinfo
 #undef curl_share_setopt
@@ -16,6 +17,16 @@
 
 /* Add the file */
 #include <curl/typecheck-gcc.h>
+#endif
+
+/* New names */
+#ifdef CURLINC_TYPECHECK_GCC_H
+#define _curl_is_string_option curlcheck_string_option
+#define _curl_is_slist_option curlcheck_slist_option
+#define _curl_is_off_t_option curlcheck_off_t_option
+#define _curl_is_long_option curlcheck_long_option
+#define _curl_is_postfields_option curlcheck_postfields_option
+#define _curl_is_cb_data_option curlcheck_cb_data_option
 #endif
 
 int r_curl_is_slist_option(CURLoption x){
