81eeb5e62b7c4f20d7a390227b038ae03cf802e3
Jeroen Ooms <jeroenooms@gmail.com>
2019-09-23 15:53:55 +0200

    Fix compiler warning

--- src/multi.c
+++ src/multi.c
@@ -244,10 +244,8 @@ SEXP R_multi_new(){
 }
 
 SEXP R_multi_setopt(SEXP pool_ptr, SEXP total_con, SEXP host_con, SEXP multiplex){
-  multiref *mref = get_multiref(pool_ptr);
-
   #ifdef HAS_CURLMOPT_MAX_TOTAL_CONNECTIONS
-    CURLM *multi = mref->m;
+    CURLM *multi = get_multiref(pool_ptr)->m;
     massert(curl_multi_setopt(multi, CURLMOPT_MAX_TOTAL_CONNECTIONS, (long) asInteger(total_con)));
     massert(curl_multi_setopt(multi, CURLMOPT_MAX_HOST_CONNECTIONS, (long) asInteger(host_con)));
   #endif
