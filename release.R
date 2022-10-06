14b69d2a9df3e343c50adb528e03b485cfd44aef
Jeroen Ooms <jeroenooms@gmail.com>
2022-10-06 18:08:16 +0100

    Cleanup old file

--- release.R
+++
@@ -1,6 +0,0 @@
-devtools::revdep_check_reset()
-devtools::revdep_check(threads = 8, env_vars = c(NOT_CRAN=FALSE),
-  ignore=c("data.table", "geoknife", "textreadr", "rgho"))
-devtools::revdep_check_print_problems()
-devtools::revdep_check_save_summary()
-devtools::spell_check()
