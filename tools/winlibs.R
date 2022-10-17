92b20fa6e20181efd16b2d61d35d4fcf952b2e3d
Jeroen Ooms <jeroenooms@gmail.com>
2022-10-17 23:05:40 +0200

    test newer libcurl on windows

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -2,7 +2,7 @@
 VERSION <- commandArgs(TRUE)
 if(!file.exists(sprintf("../windows/libcurl-%s/include/curl/curl.h", VERSION))){
   if(getRversion() < "3.3.0") setInternet2()
-  download.file(sprintf("https://github.com/rwinlib/libcurl/archive/v%s.zip", VERSION), "lib.zip", quiet = TRUE)
+  download.file(sprintf("https://github.com/rwinlib/libcurl/archive/%s.zip", VERSION), "lib.zip", quiet = TRUE)
   dir.create("../windows", showWarnings = FALSE)
   unzip("lib.zip", exdir = "../windows")
   unlink("lib.zip")
