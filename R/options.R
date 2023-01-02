b608d47304cfb35bf9db1ea0b2d38a23ae70b376
Jeroen Ooms <jeroenooms@gmail.com>
2023-01-02 22:29:27 +0100

    Minor speedup

--- R/options.R
+++ R/options.R
@@ -56,5 +56,5 @@ option_type_table <- function(){
   out$name <- tolower(out$name)
   out$type <- factor(out$type, levels = 0:8, labels = c("long", "values", "off_t",
     "object", "string", "slist", "cbptr", "blob", "function"))
-  data.frame(out, stringsAsFactors = FALSE)
+  structure(out, class = 'data.frame', row.names = seq_along(out$name))
 }
