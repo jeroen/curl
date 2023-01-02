9373632facbdb0ea138bba3c444a1cd233b39c7c
Jeroen Ooms <jeroenooms@gmail.com>
2023-01-02 21:21:31 +0100

    Fix for R 3.6

--- R/options.R
+++ R/options.R
@@ -57,5 +57,5 @@ option_type_table <- function(){
   out$name <- tolower(out$name)
   out$type <- factor(out$type, levels = 0:8, labels = c("long", "values", "off_t",
     "object", "string", "slist", "cbptr", "blob", "function"))
-  as.data.frame(out)
+  as.data.frame(out, stringsAsFactors = FALSE)
 }
