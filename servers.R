2886859dcd5b1e1dbdbbc29b2664902146016fa3
Jeroen Ooms <jeroenooms@gmail.com>
2023-06-06 23:04:34 +0200

    add new httpbin mirror

--- servers.R
+++ servers.R
@@ -1,2 +1,4 @@
-c("http://hb.opencpu.org", "https://nghttp2.org/httpbin", "https://eu.httpbin.org",
+#NB: hb.cran.dev uses cloudflare proxy which tags on some headers
+c("https://hb.r-universe.dev",
+  "http://hb.opencpu.org", "https://nghttp2.org/httpbin", "https://eu.httpbin.org",
   "https://httpbin.org", "http://httpbin.org")
