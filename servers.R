a42fb13770a4033a59ac611f09ee8e869eac319a
Jeroen Ooms <jeroenooms@gmail.com>
2023-08-10 10:22:49 +0200

    Revert "add hotfix"
    This reverts commit c664da942c702e40cf3c15f637e56928566cedcf.

--- servers.R
+++ servers.R
@@ -1,10 +1,3 @@
-# Temp workaround for bug with libcurl 8.0.0
-tryCatch({
-  lines <- readLines('test-multi.R')
-  lines <- sub("expect_length(c(fdset", "#expect_length(c(fdset", lines, fixed = TRUE)
-  writeLines(lines, 'test-multi.R')
-}, error = function(e){})
-
 #NB: hb.cran.dev uses cloudflare proxy which tags on some headers
 c("https://hb.r-universe.dev",
   "http://hb.opencpu.org", "https://nghttp2.org/httpbin", "https://eu.httpbin.org",
