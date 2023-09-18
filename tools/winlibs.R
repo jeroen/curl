ed558e538a94bfb8cf080c1b6379a69bbe9b8b76
Jeroen Ooms <jeroenooms@gmail.com>
2023-09-18 21:32:36 +0200

    Fix URLs

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,9 +1,9 @@
 if(!file.exists("../windows/libcurl/include/curl/curl.h")){
   unlink("../windows", recursive = TRUE)
   url <- if(grepl("aarch", R.version$platform)){
-    "https://github.com/r-windows/bundler/releases/download/curl-8.1.2-9000/curl-8.1.2-9000-clang-aarch64.tar.xz"
+    "https://github.com/r-windows/bundles/releases/download/curl-8.1.2/curl-8.1.2-clang-aarch64.tar.xz"
   } else if(getRversion() >= "4.2") {
-    "https://github.com/r-windows/bundler/releases/download/curl-8.1.2-9000/curl-8.1.2-9000-ucrt-x86_64.tar.xz"
+    "https://github.com/r-windows/bundles/releases/download/curl-8.1.2/curl-8.1.2-ucrt-x86_64.tar.xz"
   } else {
     "https://github.com/rwinlib/libcurl/archive/v7.84.0.tar.gz"
   }
