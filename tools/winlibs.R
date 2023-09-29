9ecfc41f4f3394f589e569968757455c970519ef
Jeroen Ooms <jeroenooms@gmail.com>
2023-09-29 14:54:30 +0200

    Add clang explicitly

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -2,6 +2,8 @@ if(!file.exists("../windows/libcurl/include/curl/curl.h")){
   unlink("../windows", recursive = TRUE)
   url <- if(grepl("aarch", R.version$platform)){
     "https://github.com/r-windows/bundles/releases/download/curl-8.1.2/curl-8.1.2-clang-aarch64.tar.xz"
+  } else if(grepl("clang", Sys.getenv('R_COMPILED_BY'))){
+    "https://github.com/r-windows/bundles/releases/download/curl-8.1.2/curl-8.1.2-clang-x86_64.tar.xz"
   } else if(getRversion() >= "4.2") {
     "https://github.com/r-windows/bundles/releases/download/curl-8.1.2/curl-8.1.2-ucrt-x86_64.tar.xz"
   } else {
