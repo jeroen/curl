ca5d30ef69d51d1156ee3d920b0350d8ae0d8a36
Jeroen Ooms <jeroenooms@gmail.com>
2025-01-13 21:04:43 +0100

    Expose max_streams parameter in new_pool

--- R/multi.R
+++ R/multi.R
@@ -172,9 +172,9 @@ multi_cancel <- function(handle){
 #' @export
 #' @useDynLib curl R_multi_new
 #' @rdname multi
-new_pool <- function(total_con = 100, host_con = 6, multiplex = TRUE){
+new_pool <- function(total_con = 100, host_con = 6, max_streams = 10, multiplex = TRUE){
   pool <- .Call(R_multi_new)
-  multi_set(pool = pool, total_con = total_con, host_con = host_con, multiplex = multiplex)
+  multi_set(pool = pool, total_con = total_con, host_con = host_con, max_streams = max_streams, multiplex = multiplex)
 }
 
 multi_default <- local({
