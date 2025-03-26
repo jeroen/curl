959f19d9ff92beab783720528d0426b123ac8089
Jeroen Ooms <jeroenooms@gmail.com>
2025-03-26 10:23:02 +0100

    Use opencpu for now

--- servers.R
+++ servers.R
@@ -1,4 +1,4 @@
 #NB: hb.cran.dev uses cloudflare proxy which tags on some headers
-c("https://hb.r-universe.dev",
-  "http://hb.opencpu.org", "https://nghttp2.org/httpbin", "https://eu.httpbin.org",
+c('https://hb.opencpu.org', "https://hb.r-universe.dev",
+  "https://nghttp2.org/httpbin", "https://eu.httpbin.org",
   "https://httpbin.org", "http://httpbin.org")
